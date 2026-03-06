4e1adbaecff364c7ecb6c2048a9da85d1a511c3c
Albert Dong <albertd@g.ucla.edu>
2026-03-06 02:02:37 -0800

    Fix API test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -11,26 +9,14 @@ function App() {
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
-    const result = await response.json();
-    setMessage((message) => message + result);
+    setMessage((message) => message + response.text);
   };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={fetchData}>click me to test the api</button>
-        <p>Response: {message}</p>
-      </div>
-    </>
+    <div className="card">
+      <button onClick={fetchData}>click me to test the api</button>
+      <p>response: {message}</p>
+    </div>
   );
 }
 
