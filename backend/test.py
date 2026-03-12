76429b5765f10b0d2de3e42cb74dc6cfc94e5690
Albert Dong <albertd@g.ucla.edu>
2026-03-11 21:54:22 -0700

    test out changing api route on dev

--- backend/test.py
+++ backend/test.py
@@ -2,6 +2,6 @@ import random
 
 
 def lambda_handler(event, context):
-    result = f"Hello World {random.randint(0, 100)}\n"
+    result = f"Hello World this should only be for testing {random.randint(0, 100)}\n"
 
     return {"statusCode": 200, "body": result}
