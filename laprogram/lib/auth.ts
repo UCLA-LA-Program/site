838a90d74866c4ca4be0984616e12d9aa572c2b5
Albert Dong <albertd@g.ucla.edu>
2026-03-29 02:14:35 -0700

    clean it up

--- laprogram/lib/auth.ts
+++ laprogram/lib/auth.ts
@@ -6,7 +6,7 @@ import { getCloudflareContext } from "@opennextjs/cloudflare";
 import { Kysely } from "kysely";
 import { D1Dialect } from "kysely-d1";
 import { nextCookies } from "better-auth/next-js";
-import sendMagicLinkSES from "./email";
+import sendMagicLink from "./email";
 
 async function authBuilder() {
   const { env } = await getCloudflareContext({ async: true });
@@ -31,7 +31,7 @@ async function authBuilder() {
       magicLink({
         disableSignUp: true,
         sendMagicLink: async (data) => {
-          await sendMagicLinkSES(data.email, data.url);
+          await sendMagicLink(data.email, data.url);
         },
       }),
       admin(),
