910e6a6fa53b2e40c66e8ce7d39e2813d0d95072
Albert Dong <albertd@g.ucla.edu>
2026-03-29 02:17:35 -0700

    Switch email provider to Postmark (#17)
    * worker dev false
    
    * change to postmark
    
    hide postmark api
    
    * check for token
    
    * add email logging for debug
    
    * try this flag
    
    * ok i named it wrong on the site
    
    * make logging a little nicer
    
    * log the emailing response
    
    * ok make sure we check that the token is ok
    
    * try using http api
    
    * erm
    
    * just try whatever at this point
    
    * if this is why its over
    
    * clean it up

--- laprogram/lib/email.ts
+++ laprogram/lib/email.ts
@@ -1,38 +1,42 @@
 import "server-only";
 
-import nodemailer from "nodemailer";
-import { SESv2Client, SendEmailCommand } from "@aws-sdk/client-sesv2";
 import { getCloudflareContext } from "@opennextjs/cloudflare";
 
 export default async function sendMagicLink(email: string, url: string) {
-  const sesClient = new SESv2Client({ region: "us-west-2" });
-
-  const transporter = nodemailer.createTransport({
-    SES: { sesClient, SendEmailCommand },
-  });
-
   const { env } = await getCloudflareContext({ async: true });
-  const user = await env.data
-    ?.prepare("SELECT id FROM user WHERE email = ?")
-    .bind(email)
-    .first();
 
-  if (!user) {
-    await transporter.sendMail({
-      from: "UCLA LA Program <login@laprogramucla.com>",
-      to: email,
-      subject: "[UCLA LA Program] — No Account",
-      text: `Someone requested a login link for the UCLA LA Program using this email address, but no account exists for ${email}.\n\nIf you believe you have an account, please try other emails before contacting PDT. The email we used to create your account was the email you used to apply to the LA Program.\n\nIf you didn't request this, you can safely ignore this email.`,
-      html: `<p>Someone requested a login link for the UCLA LA Program using this email address, but no account exists for <strong>${email}</strong>.</p><p>If you believe you have an account, please try other emails before contacting PDT. The email we used to create your account was the email you used to apply to the LA Program.</p><p>If you didn't request this, you can safely ignore this email.</p>`,
-    });
+  const name = await env.data
+    ?.prepare("SELECT id, name FROM user WHERE email = ?")
+    .bind(email)
+    .first("name");
+
+  if (!name) {
+    console.log(
+      `User with email ${email} attempted to log in; no such user found`,
+    );
     return;
   }
 
-  await transporter.sendMail({
-    from: "UCLA LA Program <login@laprogramucla.com>",
-    to: email,
-    subject: "[UCLA LA Program] - Login Link",
-    text: `Click the link below to log in to the UCLA LA Program website:\n\n${url}\n\nThis link will expire shortly. If you didn't request this, you can safely ignore this email.`,
-    html: `<p>Click the link below to log in to the UCLA LA Program website:</p><p><a href="${url}">Log in!</a></p><p>This link will expire shortly. If you didn't request this, you can safely ignore this email.</p>`,
+  if (!process.env.POSTMARK_SERVER_TOKEN) {
+    console.log("Could not load process.env.POSTMARK_SERVER_TOKEN");
+    return;
+  }
+
+  await fetch("https://api.postmarkapp.com/email/withTemplate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "X-Postmark-Server-Token": process.env.POSTMARK_SERVER_TOKEN,
+    },
+    body: JSON.stringify({
+      From: "admin@laprogramucla.com",
+      To: email,
+      TemplateId: 44160184,
+      TemplateModel: {
+        name: name,
+        action_url: url,
+      },
+    }),
   });
 }
